033c6348c0844e9991087aa4ff23272c3c803bbe
JosephIkuli <109952968+JosephIkuli@users.noreply.github.com>
2022-07-26 00:26:18 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// document.getElementById("count-el").innerText = 5
+
+// let count = 5
+
+// console.log(count)
+
+// To comment multiple lines of code: ctrl + k + c
+// let saveEl = document.getElementById("save-el")
+// let countEl = document.getElementById("count-el")
+// console.log(saveEl)
+
+// let count = 0
+
+// function increment() {
+//     count += 1
+//     countEl.textContent = count
+// }
+
+// function save() {
+//     let countStr = count + " - "
+//     saveEl.textContent += countStr
+//     countEl.textContent = 0
+//     count = 0
+// }
+
+let firstName = "Joseph"
+let lastName = "Ikuli"
+let fullName = firstName + " " + lastName
+console.log(fullName)
